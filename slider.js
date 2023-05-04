035eba0ea166859b490401d3ca219b438240be52
Antonygeo <132488485+Antonygeo@users.noreply.github.com>
2023-05-04 16:50:48 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,34 @@
+let image = document.querySelector('.img1')
+let text1 = document.querySelector('.h2')
+let para1 = document.querySelector('.pp')
+let video = document.getElementById('#vid1')
+function slider1 (x){
+    image.src="africa/wallpaperflare.com_wallpaper (10).jpg";
+    text1.textContent="Africa";
+    para1.textContent="Africa, the second largest continent, is bounded by the Mediterranean Sea, the Red Sea, the Indian Ocean, and the Atlantic Ocean.Africa's physical geography, environment and resources,and human geography can be considered separately. Africa has eight major physical regions: the Sahara, the Sahel, the Ethiopian Highlands, the savanna, the Swahili Coast, the rainforest, the African Great Lakes, and southern Africa. Some of these regions cover large bands of the continent, such as the Sahara and Sahel, while others are isolated areas, such as the Ethiopian Highlands and the Great Lakes. Each of these regions has unique animal and plant communities"
+    video1.play("asia.mp4")
+}
+
+let image2 = document.querySelector('.img1')
+let text2 = document.querySelector('.h2')
+let para2 = document.querySelector('.pp')
+function slider2 (x){
+    image2.src="asia/wallpaperflare.com_wallpaper (14).jpg";
+    text2.textContent="Asia";
+    para2.textContent="Asia is the largest of the world's continents It is also the world's most populous continent, with roughly 60 percent of the total population Asia makes up the eastern portion of the Eurasian supercontinent; Europe occupies the western portion.The border between the two continents is debated. However, most geographers define Asia's western border as an indirect line that follows the Ural Mountains, the Caucasus Mountains, and the Caspian and Black Seas. Asia is bordered by the Arctic, Pacific, and Indian Asia's physical geography, environment and resources, and human geography can be considered separately.Asia can be divided into five major physical regions: mountain systems, plateaus plains steppes, and deserts, freshwater environments, and saltwater environments.. "
+    
+}
+
+let image3 = document.querySelector('.img1')
+let text3 = document.querySelector('.h2')
+let para3 = document.querySelector('.pp')
+function slider3 (x){
+    image.src="europe/wallpaperflare.com_wallpaper (21).jpg";
+    text3.textContent="Europe";
+    para3.textContent="Europe is the second-smallest continent. The name Europe, or Europa, is believed to be of Greek originas it is the name of a princess in Greek mythology. The name Europe may also come from combining the Greek roots eur- (wide) and -op (seeing) to form the phrase “wide-gazing."
+
+}
+function fun(){
+    alert('your message recieved !!');
+}
+
